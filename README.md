1aa3bfa9f9a9ec55bcb2395b3e6777173adfde88
JOSH HAYDEN <43011361@sfcollege.edu>
2024-05-16 16:50:25 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Particle Simulator
+by Josh Hayden
+
+### Purpose
+This purpose(s) of this project are below:
+* Improving my general C programming
+* Learning aspects of physics simulation
+* Learning to use CMake and understanding how build processes work / why they're important
+* Having fun (optional)
---
+++ build/CMakeFiles/particle_simulation.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/particle_simulation.dir/main.c.obj"
+  "CMakeFiles/particle_simulation.dir/main.c.obj.d"
+  "libparticle_simulation.dll.a"
+  "particle_simulation.exe"
+  "particle_simulation.exe.manifest"
+  "particle_simulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/particle_simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
